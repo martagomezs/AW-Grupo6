eea58fff325303973916ca5c2f80d3b02a86e306
Alvaro Gomez <alvarozdp02@gmail.com>
2023-03-14 10:45:54 +0100

    Estructura Base de datos

--- docs/Practica2/Ejercicio02/includes/mysql/estructura.sql
+++ docs/Practica2/Ejercicio02/includes/mysql/estructura.sql
@@ -1,67 +1,59 @@
 /*
   Recuerda que deshabilitar la opción "Enable foreign key checks" para evitar problemas a la hora de importar el script.
 */
-DROP TABLE IF EXISTS `RolesUsuario`;
-DROP TABLE IF EXISTS `Mensajes`;
-DROP TABLE IF EXISTS `Roles`;
 DROP TABLE IF EXISTS `Usuarios`;
+DROP TABLE IF EXISTS `Artista`;
+DROP TABLE IF EXISTS `Vinilos`;
+DROP TABLE IF EXISTS `Comentarios`;
+DROP TABLE IF EXISTS `Compras`
 
-CREATE TABLE IF NOT EXISTS `Roles` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `nombre` varchar(15) COLLATE utf8mb4_general_ci NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+CREATE TABLE IF NOT EXISTS 'Usuarios' (
+  'id' int(11) NOT NULL AUTO_INCREMENT,
+  'Contraseña' varchar(70) COLLATE utf8mb4_general_ci NOT NULL,
+  'Ntarjeta' varchar(100) COLLATE utf8mb4_general_ci NOT NULL, --Hay que mirar la longitud una vez esté encriptada
+  'Nombre' varchar(50) COLLATE utf8mb4_general_ci NOT NULL,
+  'Correo' varchar(120) COLLATE utf8mb4_general_ci NOT NULL,
+  'Rol' varchar(15) COLLATE utf8mb4_general_ci NOT NULL,
+  PRIMARY KEY ('id')
+  ) ENGINE=InnoDB  DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
-CREATE TABLE IF NOT EXISTS `RolesUsuario` (
-  `usuario` int(11) NOT NULL,
-  `rol` int(11) NOT NULL,
-  PRIMARY KEY (`usuario`,`rol`),
-  KEY `rol` (`rol`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+  CREATE TABLE IF NOT EXISTS 'Artista' (
+  'id' int(11) NOT NULL AUTO_INCREMENT,
+  'Contraseña' varchar(70) COLLATE utf8mb4_general_ci NOT NULL,
+  'Ntarjeta' varchar(100) COLLATE utf8mb4_general_ci NOT NULL, --Hay que mirar la longitud una vez esté encriptada
+  'Nombre' varchar(50) COLLATE utf8mb4_general_ci NOT NULL,
+  'Correo' varchar(120) COLLATE utf8mb4_general_ci NOT NULL,
+  'Rol' varchar(15) COLLATE utf8mb4_general_ci NOT NULL,
+  'Ventas' int(20) COLLATE utf8mb4_general_ci NOT NULL,
+  PRIMARY KEY ('id')
+  ) ENGINE=InnoDB  DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
-CREATE TABLE IF NOT EXISTS `Usuarios` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `nombreUsuario` varchar(30) COLLATE utf8mb4_general_ci NOT NULL UNIQUE,
-  `password` varchar(70) COLLATE utf8mb4_general_ci NOT NULL,
-  `nombre` varchar(50) COLLATE utf8mb4_general_ci NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
-CREATE TABLE IF NOT EXISTS `Mensajes` (
-      `id` int(11) NOT NULL AUTO_INCREMENT,
-      `autor` int(11) NOT NULL,
-      `mensaje` varchar(140) NOT NULL,
-      `fechaHora` DATETIME NOT NULL,
-      `idMensajePadre` int(11) DEFAULT NULL,
-      PRIMARY KEY (`id`)
-    ) ENGINE=InnoDB  DEFAULT CHARSET=utf8mb4  COLLATE=utf8mb4_general_ci;
-
-ALTER TABLE `RolesUsuario`
-  ADD CONSTRAINT `RolesUsuario_usuario` FOREIGN KEY (`usuario`) REFERENCES `Usuarios` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
-  ADD CONSTRAINT `RolesUsuario_rol` FOREIGN KEY (`rol`) REFERENCES `Roles` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
-
-ALTER TABLE `Mensajes`
-  ADD CONSTRAINT `Mensajes_mensaje` FOREIGN KEY (`idMensajePadre`) REFERENCES `Mensajes` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
-  ADD CONSTRAINT `Mensajes_autor` FOREIGN KEY (`autor`) REFERENCES `Usuarios` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
-
-
-
-CREATE TABLE IF NOT EXISTS `vinilos` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `titulo` varchar(50) COLLATE utf8mb4_general_ci NOT NULL UNIQUE,
-  `autor` varchar(50) COLLATE utf8mb4_general_ci NOT NULL,
-  `pistas` TEXT,
-  `muestra_audio` TEXT,
-  `portada` VARCHAR(255) NOT NULL,
-  PRIMARY KEY (`id`)
+CREATE TABLE IF NOT EXISTS 'Vinilos' (
+  'id' int(11) NOT NULL AUTO_INCREMENT,
+  'titulo' varchar(50) COLLATE utf8mb4_general_ci NOT NULL,
+  'autor' varchar(15) COLLATE utf8mb4_general_ci NOT NULL,
+  'precio' INT(15) NOT NULL,
+  'canciones' BLOB NOT NULL,
+  'portada' BLOB NOT NULL,
+  PRIMARY KEY ('id')
 ) ENGINE=InnoDB  DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 
-CREATE TABLE IF NOT EXISTS `comentarios` (
-    `id` INT(30) NOT NULL AUTO_INCREMENT,
-    `vinilo_id` INT NOT NULL,
-    `nombre` VARCHAR(50) NOT NULL,
-    `comentario` TEXT NOT NULL,
-    `fecha` TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    PRIMARY KEY (`id`)
+CREATE TABLE IF NOT EXISTS 'Comentarios' (
+    'id' INT(30) NOT NULL AUTO_INCREMENT,
+    'vinilo_id' INT NOT NULL,
+    'autor' VARCHAR(50) NOT NULL,
+    'comentario' TEXT NOT NULL,
+    'fecha' TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    PRIMARY KEY ('id'),
+    FOREIGN KEY ('autor') REFERENCES 'Usuarios'('Nombre'),
+    FOREIGN KEY ('vinilo_id') REFERENCES 'Vinilos'('id')
 ); ENGINE=InnoDB  DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+CREATE TABLE IF NOT EXISTS 'Compras'(
+  'Id-User' INT(11) NOT NULL,
+  'Id-Vinilo' INT(11) NOT NULL,
+  PRIMARY KEY ('Id-User', 'Id-Vinilo'),
+  FOREIGN KEY ('Id-User') REFERENCES 'Usuarios'('id'),
+  FOREIGN KEY ('Id-Vinilo') REFERENCES 'Vinilos'('id')
+)
