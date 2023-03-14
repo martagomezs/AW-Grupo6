7f3362b7e19f21365150179151232afd33b323c1
Alvaro Gomez <alvarozdp02@gmail.com>
2023-03-14 21:56:10 +0100

    Estructura y datos sql

--- docs/Practica2/Ejercicio02/includes/mysql/datos.sql
+++ docs/Practica2/Ejercicio02/includes/mysql/datos.sql
@@ -1,43 +1,19 @@
-/*
-  Recuerda que deshabilitar la opción "Enable foreign key checks" para evitar problemas a la hora de importar el script.
-*/
-TRUNCATE TABLE `RolesUsuario`;
-TRUNCATE TABLE `Roles`;
-TRUNCATE TABLE `Usuarios`;
-TRUNCATE TABLE `Mensajes`;
-
-INSERT INTO `Roles` (`id`, `nombre`) VALUES
-(1, 'admin'),
-(2, 'user');
 
 
-INSERT INTO `RolesUsuario` (`usuario`, `rol`) VALUES
-(1, 1),
-(1, 2),
-(2, 2);
+INSERT INTO `Usuarios` (`id`, `password`, `Ntarjeta`,`Nombre`,`Correo`,`Rol`,`Ventas`) VALUES
+(1, 'userpass', '1234567898765432', 'Usuario', 'user@usuario.com', 'usuario', NULL),
+(2, 'adminpass', '1357975313579753', 'Administrador', 'admin@administrador.com', 'admin', NULL),
+(3, 'rosi', '9876543212345678', 'Rosalia', 'moto@mami.es', 'artista', 1),
+(4, 'pink', '2468086424680864', 'Pink Floyd', 'pink@floyd.com', 'artista', 0),
+(5, 'nirvpass', '1111111111111111', 'Nirvana', 'nirvana@never.com', 'artista', 0);
 
-/*
-  user: userpass
-  admin: adminpass
-*/
-INSERT INTO `Usuarios` (`id`, `nombreUsuario`, `nombre`, `password`) VALUES
-(1, 'admin', 'Administrador', '$2y$10$O3c1kBFa2yDK5F47IUqusOJmIANjHP6EiPyke5dD18ldJEow.e0eS'),
-(2, 'user', 'Usuario', '$2y$10$uM6NtF.f6e.1Ffu2rMWYV.j.X8lhWq9l8PwJcs9/ioVKTGqink6DG');
+INSERT INTO `vinilos` (`id`, `titulo`, `autor`, `precio`, `canciones`, `portada`) VALUES 
+(1, 'The Dark Side Of The Moon', 4, 50, 'https://ejemplo.com/audio.mp3', 'portadas/id1.jpg'),
+(2, 'Motomami', 3, 35, 'https://ejemplo.com/audio.mp3', 'portadas/id2.jpg'),
+(3, 'Nevermind', 5, 40, 'https://ejemplo.com/audio.mp3', 'portadas/id3.jpg');
 
-SET @INICIO := NOW();
-INSERT INTO `Mensajes` (`id`, `autor`, `mensaje`, `fechaHora`, `idMensajePadre`) VALUES
-(1, 1, 'Bienvenido al foro', @INICIO, NULL),
-(2, 2, 'Muchas gracias', ADDTIME(@INICIO, '0:15:0'), 1),
-(3, 2, 'Otro mensaje', ADDTIME(@INICIO, '25:15:0'), NULL);
+INSERT INTO `Comentarios` (`id`,`vinilo_id`,`autor`,`comentario`,`fecha`,`padre`) VALUES
+(1, 2, 1, 'Muy buen disco', @INICIO, NULL),
+(2, 2, 3, 'Muchas gracias!!', ADDTIME(@INICIO, '0:10:0'),1);
 
-/*
-  VINILOS
-*/
-INSERT INTO vinilos (id, titulo, autor, pistas, muestra_audio, portada)
-VALUES ('1', 'The Dark Side Of The Moon', 'Pink Floyd','Speak To Me, Breathe, Time', 'https://ejemplo.com/audio.mp3', 'portadas/id1.jpg');
-
-INSERT INTO vinilos (id, titulo, autor, pistas, muestra_audio, portada)
-VALUES ('2', 'Motomami', 'Rosalía','Saoko, La Fama', 'https://ejemplo.com/audio.mp3', 'portadas/id2.jpg');
-
-INSERT INTO vinilos (id, titulo, autor, pistas, muestra_audio, portada)
-VALUES ('3', 'Nevermind', 'Nirvana','In Bloom, Breed, Lithium', 'https://ejemplo.com/audio.mp3', 'portadas/id3.jpg');
+INSERT INTO `Compras`(`Id-User`,`Id-Vinilo`) VALUES (1,2);
--- docs/Practica2/Ejercicio02/includes/mysql/estructura.sql
+++ docs/Practica2/Ejercicio02/includes/mysql/estructura.sql
@@ -5,55 +5,48 @@ DROP TABLE IF EXISTS `Usuarios`;
 DROP TABLE IF EXISTS `Artista`;
 DROP TABLE IF EXISTS `Vinilos`;
 DROP TABLE IF EXISTS `Comentarios`;
-DROP TABLE IF EXISTS `Compras`
+DROP TABLE IF EXISTS `Compras`;
 
-CREATE TABLE IF NOT EXISTS 'Usuarios' (
-  'id' int(11) NOT NULL AUTO_INCREMENT,
-  'Contraseña' varchar(70) COLLATE utf8mb4_general_ci NOT NULL,
-  'Ntarjeta' varchar(100) COLLATE utf8mb4_general_ci NOT NULL, --Hay que mirar la longitud una vez esté encriptada
-  'Nombre' varchar(50) COLLATE utf8mb4_general_ci NOT NULL,
-  'Correo' varchar(120) COLLATE utf8mb4_general_ci NOT NULL,
-  'Rol' varchar(15) COLLATE utf8mb4_general_ci NOT NULL,
-  PRIMARY KEY ('id')
+CREATE TABLE IF NOT EXISTS `Usuarios` (
+  `id` INT (11) NOT NULL AUTO_INCREMENT,
+  `password` varchar(70) COLLATE utf8mb4_general_ci NOT NULL,
+  `Ntarjeta` varchar(100) COLLATE utf8mb4_general_ci NOT NULL,
+  `Nombre` varchar(50) COLLATE utf8mb4_general_ci NOT NULL,
+  `Correo` varchar(120) COLLATE utf8mb4_general_ci NOT NULL,
+  `Rol` varchar(15) COLLATE utf8mb4_general_ci NOT NULL,
+  `Ventas` INT (20),
+  PRIMARY KEY (`id`)
   ) ENGINE=InnoDB  DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
-  CREATE TABLE IF NOT EXISTS 'Artista' (
-  'id' int(11) NOT NULL AUTO_INCREMENT,
-  'Contraseña' varchar(70) COLLATE utf8mb4_general_ci NOT NULL,
-  'Ntarjeta' varchar(100) COLLATE utf8mb4_general_ci NOT NULL, --Hay que mirar la longitud una vez esté encriptada
-  'Nombre' varchar(50) COLLATE utf8mb4_general_ci NOT NULL,
-  'Correo' varchar(120) COLLATE utf8mb4_general_ci NOT NULL,
-  'Rol' varchar(15) COLLATE utf8mb4_general_ci NOT NULL,
-  'Ventas' int(20) COLLATE utf8mb4_general_ci NOT NULL,
-  PRIMARY KEY ('id')
-  ) ENGINE=InnoDB  DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
-CREATE TABLE IF NOT EXISTS 'Vinilos' (
-  'id' int(11) NOT NULL AUTO_INCREMENT,
-  'titulo' varchar(50) COLLATE utf8mb4_general_ci NOT NULL,
-  'autor' varchar(15) COLLATE utf8mb4_general_ci NOT NULL,
-  'precio' INT(15) NOT NULL,
-  'canciones' BLOB NOT NULL,
-  'portada' BLOB NOT NULL,
-  PRIMARY KEY ('id')
+CREATE TABLE IF NOT EXISTS `Vinilos` (
+  `id` INT (11) NOT NULL AUTO_INCREMENT,
+  `titulo` varchar(50) COLLATE utf8mb4_general_ci NOT NULL,
+  `autor` INT(11) COLLATE utf8mb4_general_ci NOT NULL,
+  `precio` INT(15) NOT NULL,
+  `canciones` BLOB NOT NULL,
+  `portada` BLOB NOT NULL,
+  PRIMARY KEY (`id`),
+  FOREIGN KEY (`autor`) REFERENCES `Usuarios`(`id`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 
-CREATE TABLE IF NOT EXISTS 'Comentarios' (
-    'id' INT(30) NOT NULL AUTO_INCREMENT,
-    'vinilo_id' INT NOT NULL,
-    'autor' VARCHAR(50) NOT NULL,
-    'comentario' TEXT NOT NULL,
-    'fecha' TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    PRIMARY KEY ('id'),
-    FOREIGN KEY ('autor') REFERENCES 'Usuarios'('Nombre'),
-    FOREIGN KEY ('vinilo_id') REFERENCES 'Vinilos'('id')
+CREATE TABLE IF NOT EXISTS `Comentarios` (
+    `id` INT(11) NOT NULL AUTO_INCREMENT,
+    `vinilo_id` INT(11) NOT NULL,
+    `autor` INT(11) NOT NULL,
+    `comentario` TEXT NOT NULL,
+    `fecha` TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    `padre` INT(11),
+    PRIMARY KEY (`id`),
+    FOREIGN KEY (`autor`) REFERENCES `Usuarios`(`id`),
+    FOREIGN KEY (`vinilo_id`) REFERENCES `Vinilos`(`id`),
+    FOREIGN KEY (`padre`) REFERENCES `Comentarios`(`id`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
-CREATE TABLE IF NOT EXISTS 'Compras'(
-  'Id-User' INT(11) NOT NULL,
-  'Id-Vinilo' INT(11) NOT NULL,
-  PRIMARY KEY ('Id-User', 'Id-Vinilo'),
-  FOREIGN KEY ('Id-User') REFERENCES 'Usuarios'('id'),
-  FOREIGN KEY ('Id-Vinilo') REFERENCES 'Vinilos'('id')
+CREATE TABLE IF NOT EXISTS `Compras`(
+  `Id-User` INT(11) NOT NULL,
+  `Id-Vinilo` INT(11) NOT NULL,
+  PRIMARY KEY (`Id-User`, `Id-Vinilo`),
+  FOREIGN KEY (`Id-User`) REFERENCES `Usuarios`(`id`),
+  FOREIGN KEY (`Id-Vinilo`) REFERENCES `Vinilos`(`id`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
